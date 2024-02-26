7566682004c358677d2330f38867c635054eecd7
omar <iamomarjr@gmail.com>
2024-02-26 17:50:14 +0300

    login configartions and relative jsp files

--- src/main/java/com/example/SpringBoot/Data/GradesDAO.java
+++ src/main/java/com/example/SpringBoot/Data/GradesDAO.java
@@ -1,60 +1,18 @@
 package com.example.SpringBoot.Data;
 
 import com.example.SpringBoot.Entities.Grade;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@Repository
-public class GradesDAO implements DAO {
+public interface GradesDAO {
+    List<Grade> getAllGrades();
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    void addGrade(Grade grade);
 
-    @Override
-    public List<Grade> getAllGrades() {
-        String query = "SELECT * FROM grades";
-        return jdbcTemplate.query(query, new GradeRowMapper());
-    }
+    void updateGrade(Grade grade);
 
-    @Override
-    public Grade getGradeById(int gradeId) {
-        String query = "SELECT * FROM grades WHERE grade_id = ?";
-        return jdbcTemplate.queryForObject(query, new Object[]{gradeId}, new GradeRowMapper());
-    }
+    void deleteGrade(int gradeId);
 
-    @Override
-    public void addGrade(Grade grade) {
-        String query = "INSERT INTO grades (student_id, course_id, grade) VALUES (?, ?, ?)";
-        jdbcTemplate.update(query, grade.getStudentId(), grade.getCourseId(), grade.getGrade());
-    }
+    Grade getGradeById(int gradeId);
 
-    @Override
-    public void updateGrade(Grade grade) {
-        String query = "UPDATE grades SET student_id = ?, course_id = ?, grade = ? WHERE grade_id = ?";
-        jdbcTemplate.update(query, grade.getStudentId(), grade.getCourseId(), grade.getGrade(), grade.getGradeId());
-    }
-
-    @Override
-    public void deleteGrade(int gradeId) {
-        String query = "DELETE FROM grades WHERE grade_id = ?";
-        jdbcTemplate.update(query, gradeId);
-    }
-
-    private static class GradeRowMapper implements RowMapper<Grade> {
-        @Override
-        public Grade mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Grade grade = new Grade();
-            grade.setGradeId(rs.getInt("grade_id"));
-            grade.setStudentId(rs.getInt("student_id"));
-            grade.setCourseId(rs.getInt("course_id"));
-            grade.setGrade(rs.getDouble("grade"));
-            return grade;
-        }
-    }
 }
